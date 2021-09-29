d50631c7b4d93eebc8d913136ba26b0673baec55
tobeor3009 <tobeor3009@gmail.com>
2021-09-29 19:40:19 +0900

    daily commit

--- src/model/pvt_v2.py
+++ src/model/pvt_v2.py
@@ -61,28 +61,35 @@ class LayerArchive:
         pass
 
 
+def drop_path_(inputs, drop_prob, is_training):
+
+    # Bypass in non-training mode
+    if (not is_training) or (drop_prob == 0.):
+        return inputs
+
+    # Compute keep_prob
+    keep_prob = 1.0 - drop_prob
+
+    # Compute drop_connect tensor
+    input_shape = tf.shape(inputs)
+    batch_num = input_shape[0]
+    rank = len(input_shape)
+
+    shape = (batch_num,) + (1,) * (rank - 1)
+    random_tensor = keep_prob + tf.random.uniform(shape, dtype=inputs.dtype)
+    path_mask = tf.floor(random_tensor)
+    output = tf.math.divide(inputs, keep_prob) * path_mask
+    return output
+
+
 class DropPath(layers.Layer):
-    def __init__(self, drop_prob=0., training=False):
-        super(DropPath, self).__init__()
+    def __init__(self, drop_prob=None, training=None):
+        super().__init__()
         self.drop_prob = drop_prob
         self.training = training
-        self.keep_prob = 1 - self.drop_prob
-
-    def build(self, input_shape):
-        batch_size = input_shape[0]
-        ndim = len(input_shape)
-        self.shape = (batch_size, ) + (1, ) * (ndim - 1)
 
     def call(self, x):
-        if self.drop_prob == 0. or not self.training:
-            return x
-
-        random_tensor = self.keep_prob + \
-            tf.random.normal(self.shape, 0.0, 1.0)
-        random_tensor = tf.math.floor(random_tensor)
-        output = (x / self.keep_prob) * random_tensor
-
-        return output
+        return drop_path_(x, self.drop_prob, self.training)
 
 
 class Mlp(layers.Layer):
