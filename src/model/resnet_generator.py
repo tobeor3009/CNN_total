5ddaaed6ba5a61498c404686beacfe68bcce96dc
tobeor3009 <tobeor3009@gmail.com>
2021-12-01 14:04:02 +0900

    update cyclegan

--- src/model/resnet_generator.py
+++ src/model/resnet_generator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import tensorflow_addons as tfa
 from tensorflow.keras import backend as keras_backend
 from tensorflow.keras import layers, activations, Sequential, Model
 from tensorflow.keras.initializers import RandomNormal
@@ -112,36 +113,24 @@ class ReflectionPadding2D(layers.Layer):
                       mode="REFLECT")
 
 
-class UnshapeMasking2D(layers.Layer):
-    def __init__(self, in_channel, out_channel):
-        super(UnshapeMasking2D, self).__init__()
-        unsharp_kernel = [[0, -1, 0],
-                          [-1, 5, -1],
-                          [0, -1, 0]]
-        unsharp_kernel = [[1, 4, 6, 4, 1],
-                          [4, 16, 24, 16, 4],
-                          [6, 24, -476, 24, 6],
-                          [4, 16, 24, 16, 4],
-                          [1, 4, 6, 4, 1]]
-
-        kernel = []
-        kernel_row = []
-
-        for _ in range(in_channel):
-            kernel_row.append(unsharp_kernel)
-
-        for _ in range(out_channel):
-            kernel.append(kernel_row)
-
-        self.unsharp_kernel = keras_backend.constant(np.array(kernel).T / -256)
+class UnsharpMasking2D(layers.Layer):
+    def __init__(self):
+        super(UnsharpMasking2D, self).__init__()
+        gauss_kernel_2d = get_gaussian_kernel(2, 0.0, 1.0)
+        self.gauss_kernel = tf.tile(
+            gauss_kernel_2d[:, :, tf.newaxis, tf.newaxis], [1, 1, 3, 1])
+        self.pointwise_filter = tf.eye(3, batch_shape=[1, 1])
 
     def call(self, input_tensor):
 
-        return tf.nn.convolution(input=input_tensor,
-                                 filters=self.unsharp_kernel,
-                                 strides=1,
-                                 padding="SAME"
-                                 )
+        blur_tensor = tf.nn.separable_conv2d(input_tensor,
+                                             self.gauss_kernel,
+                                             self.pointwise_filter,
+                                             strides=[1, 1, 1, 1], padding='SAME')
+        unsharp_mask_tensor = 2 * input_tensor - blur_tensor
+        # because it used after tanh
+        unsharp_mask_tensor = tf.clip_by_value(unsharp_mask_tensor, -1, 1)
+        return unsharp_mask_tensor
 
 
 def DecoderTransposeX2Block(filters):
@@ -243,7 +232,7 @@ class HighwayResnetEncoder(layers.Layer):
 class HighwayResnetDecoder(layers.Layer):
     def __init__(self, filters):
         super(HighwayResnetDecoder, self).__init__()
-        self.unsharp_mask_layer = UnshapeMasking2D(filters, filters)
+        self.unsharp_mask_layer = UnsharpMasking2D()
         # Define Base Model Params
         kernel_init = RandomNormal(mean=0.0, stddev=0.02)
         self.padding_layer = ReflectionPadding2D()
@@ -321,9 +310,7 @@ def get_highway_resnet_generator_unet(input_shape,
             init_filters * index)(encoded_tensor)
         encoder_tensor_list.append(encoded_tensor)
 
-    feature_selcted_tensor = middle_layers(encoded_tensor)
-
-    decoded_tensor = feature_selcted_tensor
+    decoded_tensor = middle_layers(encoded_tensor)
 
     for index in range(decoder_depth, 0, -1):
 
@@ -428,3 +415,52 @@ def get_highway_resnet_generator_stargan_unet(input_shape, label_len, target_lab
                                          padding="same", kernel_initializer=kernel_init)(last_modified_tensor)
     last_modified_tensor = activations.tanh(last_modified_tensor)
     return Model([input_tensor, class_input, target_class_input], last_modified_tensor)
+
+
+def get_discriminator(
+    input_img_shape,
+    output_img_shape,
+    depth=None,
+    init_filters=32,
+):
+
+    # this model output range [0, 1]. control by ResidualLastBlock's sigmiod activation
+
+    original_image = layers.Input(shape=input_img_shape)
+    predicted_image = layers.Input(shape=output_img_shape)
+    # Concatenate image and conditioning image by channels to produce input
+    combined_imgs = layers.Concatenate(
+        axis=-1)([original_image, predicted_image])
+
+    if depth is None:
+        img_size = input_img_shape[0]
+        depth = 0
+        while img_size != 1:
+            img_size //= 2
+            depth += 1
+        depth -= 3
+    decoded_tensor = HighwayResnetBlock(
+        init_filters, use_highway=False)(combined_imgs)
+    for depth_step in range(depth):
+        decoded_tensor = HighwayResnetBlock(
+            init_filters * (2 ** ((depth_step + 1) // 2)), use_highway=False)(decoded_tensor)
+        decoded_tensor = HighwayResnetBlock(
+            init_filters * (2 ** ((depth_step + 1) // 2)), use_highway=True)(decoded_tensor)
+        decoded_tensor = HighwayResnetBlock(
+            init_filters * (2 ** ((depth_step + 1) // 2)), use_highway=False)(decoded_tensor)
+
+    validity = HighwayResnetBlock(1, use_highway=False)(decoded_tensor)
+    validity = activations.sigmoid(validity)
+    return Model([original_image, predicted_image], validity)
+
+
+def get_gaussian_kernel(size=2, mean=0.0, std=1.0):
+    """Makes 2D gaussian Kernel for convolution."""
+
+    d = tf.compat.v1.distributions.Normal(mean, std)
+    vals = d.prob(tf.range(start=-size, limit=size + 1, dtype=tf.float32))
+    gauss_kernel = tf.einsum('i,j->ij',
+                             vals,
+                             vals)
+
+    return gauss_kernel / tf.reduce_sum(gauss_kernel)
