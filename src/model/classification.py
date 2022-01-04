dd4871ee0377ed3a9f72fa1a2900ecd59d95f0f2
tobeor3009 <tobeor3009@gmail.com>
2022-01-04 13:04:17 +0900

    daily commit

--- src/model/classification.py
+++ src/model/classification.py
@@ -1,6 +1,7 @@
+from .coord import CoordinateChannel2D
+from tensorflow.keras import backend as keras_backend
 from tensorflow.keras.applications import InceptionV3
-from tensorflow.keras.losses import CategoricalCrossentropy, BinaryCrossentropy
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tensorflow.keras import layers
 from tensorflow.keras.models import Model
 
 import numpy as np
@@ -8,61 +9,63 @@ np.random.seed(1337)  # for reproducibility
 
 DROPOUT_RATIO = 0.5
 
-grad_cam = False
-transfer_learning = False
-transfer_train_mode = "include_deep_layer"
-layer_name_frozen_to = "mixed4"
 
-#  binary_sigmoid, categorical_sigmoid, categorical_softmax
-activation = "binary_sigmoid"
+def get_inceptionv3_classification_model(input_shape, num_class,
+                                         activation="binary_sigmoid",
+                                         grad_cam=False,
+                                         transfer_learning=False, train_mode="include_deep_layer",
+                                         layer_name_frozen_to="mixed4",
+                                         ):
 
-# create the base pre-trained model~
-base_model = InceptionV3(
-    include_top=False,
-    weights="imagenet",
-    input_tensor=None,
-    input_shape=(None,None,3),
-    classes=None,
-    pooling=None,
-    classifier_activation=None
-)
+    base_input = layers.Input(shape=(input_shape))
+    coordinate_input = CoordinateChannel2D(use_radius=True)(base_input)
+    # create the base pre-trained model~
+    base_model = InceptionV3(
+        include_top=False,
+        weights=None,
+        input_tensor=None,
+        input_shape=(input_shape[0], input_shape[1], input_shape[2] + 3),
+        classes=None,
+        pooling=None,
+        classifier_activation=None
+    )
+    if transfer_learning:
+        if train_mode == "dense_only":
+            base_model.trainable = False
+        elif train_mode == "include_deep_layer":
+            for layer in base_model.layers:
+                layer.trainable = False
+                if layer.name == layer_name_frozen_to:
+                    break
 
-if transfer_learning:
-    if train_mode == "dense_only":
-        base_model.trainable = False
-    elif train_mode == "include_deep_layer":
-        for layer in base_model.layers: 
-            layer.trainable = False
-            if layer.name == layer_name_frozen_to:
-                break
+    # add a global spatial average pooling layer
+    x = base_model(coordinate_input)
+    # (Batch_Size,?)
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Dropout(DROPOUT_RATIO)(x)
+    # let's add a fully-connected layer
+    # (Batch_Size,1)
+    x = layers.Dense(1024, activation='relu')(x)
+    # (Batch_Size,1024)
+    x = layers.Dropout(DROPOUT_RATIO)(x)
 
-# add a global spatial average pooling layer
-x = base_model.output
-# (Batch_Size,?)
-x = GlobalAveragePooling2D()(x)
-x = Dropout(DROPOUT_RATIO)(x)
-# let's add a fully-connected layer
-# (Batch_Size,1)
-x = Dense(1024, activation='relu')(x)
-# (Batch_Size,1024)
-x = Dropout(DROPOUT_RATIO)(x)
+    if grad_cam:
+        x *= 1e-1
+        keras_backend.set_floatx('float64')
+        dense_dtype = "float64"
+    else:
+        dense_dtype = "float32"
 
-if grad_cam:
-    x *= 1e-1
-    keras_backend.set_floatx('float64')
-    dense_dtype = "float64"
-else:
-    dense_dtype = "float32"
-    
-if activation == "binary_sigmoid":
-    predictions = Dense(1, activation='sigmoid', dtype=dense_dtype)(x)
-    loss_function = BinaryCrossentropy(label_smoothing=0.01)
-elif activation == "categorical_sigmoid":
-    predictions = Dense(len(label_list), activation='sigmoid', dtype=dense_dtype)(x)
-    loss_function = CategoricalCrossentropy(label_smoothing=0.01)
-elif activation == "categorical_softmax":
-    predictions = Dense(len(label_list), activation='softmax', dtype=dense_dtype)(x)
-    loss_function = CategoricalCrossentropy(label_smoothing=0.01)
+    if activation == "binary_sigmoid":
+        predictions = layers.Dense(
+            1, activation='sigmoid', dtype=dense_dtype)(x)
+    elif activation == "categorical_sigmoid":
+        predictions = layers.Dense(
+            num_class, activation='sigmoid', dtype=dense_dtype)(x)
+    elif activation == "categorical_softmax":
+        predictions = layers.Dense(
+            num_class, activation='softmax', dtype=dense_dtype)(x)
 
-# this is the model we will train
-model = Model(base_model.input, predictions)
+    # this is the model we will train
+    model = Model(base_input, predictions)
+    return model
