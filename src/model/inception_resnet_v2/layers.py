46d06e211f2b7240dffe7cb2cc07236fc70b2a62
tobeor3009 <tobeor3009@gmail.com>
2022-02-15 13:51:38 +0900

    daily commit

---
+++ src/model/inception_resnet_v2/layers.py
@@ -0,0 +1,297 @@
+import tensorflow as tf
+from tensorflow.keras import backend
+from tensorflow.keras import layers, Sequential
+from tensorflow.keras.initializers import Constant
+from tensorflow.keras.activations import tanh
+
+USE_CONV_BIAS = True
+USE_DENSE_BIAS = True
+
+
+def conv3d_bn(x,
+              filters,
+              kernel_size,
+              strides=1,
+              padding='same',
+              activation='relu',
+              use_bias=False,
+              name=None):
+    """Utility function to apply conv + BN.
+
+    Args:
+      x: input tensor.
+      filters: filters in `Conv2D`.
+      kernel_size: kernel size as in `Conv2D`.
+      strides: strides in `Conv2D`.
+      padding: padding mode in `Conv2D`.
+      activation: activation in `Conv2D`.
+      use_bias: whether to use a bias in `Conv2D`.
+      name: name of the ops; will become `name + '_ac'` for the activation
+          and `name + '_bn'` for the batch norm layer.
+
+    Returns:
+      Output tensor after applying `Conv2D` and `BatchNormalization`.
+    """
+    x = layers.Conv3D(
+        filters,
+        kernel_size,
+        strides=strides,
+        padding=padding,
+        use_bias=use_bias,
+        name=name)(
+            x)
+    if not use_bias:
+        bn_axis = -1
+        bn_name = None if name is None else name + '_bn'
+        x = layers.BatchNormalization(
+            axis=bn_axis, scale=False, name=bn_name)(x)
+    if activation is not None:
+        ac_name = None if name is None else name + '_ac'
+        if activation == 'relu':
+            x = layers.Activation(tf.nn.relu6, name=ac_name)(x)
+        else:
+            x = layers.Activation(activation, name=ac_name)(x)
+    return x
+
+
+def inception_resnet_block_3d(x, scale, block_type, block_idx, activation='relu'):
+    """Adds an Inception-ResNet block.
+
+    This function builds 3 types of Inception-ResNet blocks mentioned
+    in the paper, controlled by the `block_type` argument (which is the
+    block name used in the official TF-slim implementation):
+    - Inception-ResNet-A: `block_type='block35'`
+    - Inception-ResNet-B: `block_type='block17'`
+    - Inception-ResNet-C: `block_type='block8'`
+
+    Args:
+      x: input tensor.
+      scale: scaling factor to scale the residuals (i.e., the output of passing
+        `x` through an inception module) before adding them to the shortcut
+        branch. Let `r` be the output from the residual branch, the output of this
+        block will be `x + scale * r`.
+      block_type: `'block35'`, `'block17'` or `'block8'`, determines the network
+        structure in the residual branch.
+      block_idx: an `int` used for generating layer names. The Inception-ResNet
+        blocks are repeated many times in this network. We use `block_idx` to
+        identify each of the repetitions. For example, the first
+        Inception-ResNet-A block will have `block_type='block35', block_idx=0`,
+        and the layer names will have a common prefix `'block35_0'`.
+      activation: activation function to use at the end of the block (see
+        [activations](../activations.md)). When `activation=None`, no activation
+        is applied
+        (i.e., "linear" activation: `a(x) = x`).
+
+    Returns:
+        Output tensor for the block.
+
+    Raises:
+      ValueError: if `block_type` is not one of `'block35'`,
+        `'block17'` or `'block8'`.
+    """
+    if block_type == 'block35_3d':
+        branch_0 = conv3d_bn(x, 160, 1)
+        branch_1 = conv3d_bn(x, 160, 1)
+        branch_1 = conv3d_bn(branch_1, 160, 3)
+        branch_2 = conv3d_bn(x, 160, 1)
+        branch_2 = conv3d_bn(branch_2, 240, 3)
+        branch_2 = conv3d_bn(branch_2, 320, 3)
+        branches = [branch_0, branch_1, branch_2]
+    elif block_type == 'block17_3d':
+        branch_0 = conv3d_bn(x, 288, 1)
+        branch_1 = conv3d_bn(x, 192, 1)
+        branch_1 = conv3d_bn(branch_1, 240, [1, 1, 7])
+        branch_1 = conv3d_bn(branch_1, 264, [1, 7, 1])
+        branch_1 = conv3d_bn(branch_1, 288, [7, 1, 1])
+        branches = [branch_0, branch_1]
+    elif block_type == 'block8_3d':
+        branch_0 = conv3d_bn(x, 192, 1)
+        branch_1 = conv3d_bn(x, 192, 1)
+        branch_1 = conv3d_bn(branch_1, 224, [1, 1, 3])
+        branch_1 = conv3d_bn(branch_1, 240, [1, 3, 1])
+        branch_1 = conv3d_bn(branch_1, 256, [3, 1, 1])
+        branches = [branch_0, branch_1]
+    else:
+        raise ValueError('Unknown Inception-ResNet block type. '
+                         'Expects "block35_3d", "block17_3d" or "branch_1", '
+                         'but got: ' + str(block_type))
+
+    block_name = block_type + '_' + str(block_idx)
+    channel_axis = -1
+    mixed = layers.Concatenate(
+        axis=channel_axis, name=block_name + '_mixed')(
+            branches)
+    up = conv3d_bn(
+        mixed,
+        backend.int_shape(x)[channel_axis],
+        1,
+        activation=None,
+        use_bias=True,
+        name=block_name + '_conv')
+
+    x = layers.Lambda(
+        lambda inputs, scale: inputs[0] + inputs[1] * scale,
+        output_shape=backend.int_shape(x)[1:],
+        arguments={'scale': scale},
+        name=block_name)([x, up])
+    if activation is not None:
+        if activation == 'relu':
+            x = layers.Activation(tf.nn.relu6, name=block_name + '_ac')(x)
+        else:
+            x = layers.Activation(activation, name=block_name + '_ac')(x)
+    return x
+
+
+class SkipUpsample3D(layers.Layer):
+    def __init__(self, filters):
+        super().__init__()
+        self.compress_block = Sequential([
+            layers.Conv2D(filters, kernel_size=1, padding="same",
+                          strides=1, use_bias=USE_CONV_BIAS),
+            layers.BatchNormalization(axis=-1),
+            layers.Activation("tanh")
+        ])
+        self.conv_block = Sequential([
+            layers.Conv3D(filters, kernel_size=3, padding="same",
+                          strides=1, use_bias=USE_CONV_BIAS),
+            layers.BatchNormalization(axis=-1),
+            layers.Activation("tanh")
+        ])
+
+    def call(self, input_tensor, H):
+        conv = self.compress_block(input_tensor)
+        # shape: [B H W 1 C]
+        conv = backend.expand_dims(conv, axis=-2)
+        conv = backend.repeat_elements(conv, rep=H, axis=-2)
+        conv = self.conv_block(conv)
+        return conv
+
+
+class HighwayMulti(layers.Layer):
+
+    activation = None
+    transform_gate_bias = None
+
+    def __init__(self, dim, activation='relu', transform_gate_bias=-3, **kwargs):
+        self.activation = activation
+        self.transform_gate_bias = transform_gate_bias
+        transform_gate_bias_initializer = Constant(self.transform_gate_bias)
+        self.dim = dim
+        self.dense_1 = layers.Dense(units=self.dim,
+                                    use_bias=USE_DENSE_BIAS, bias_initializer=transform_gate_bias_initializer)
+
+        super(HighwayMulti, self).__init__(**kwargs)
+
+    def call(self, x, y):
+
+        transform_gate = layers.GlobalAveragePooling3D()(x)
+        transform_gate = self.dense_1(transform_gate)
+        transform_gate = layers.Activation("sigmoid")(transform_gate)
+        carry_gate = layers.Lambda(lambda x: 1.0 - x,
+                                   output_shape=(self.dim,))(transform_gate)
+        transformed_gated = layers.Multiply()([transform_gate, x])
+        identity_gated = layers.Multiply()([carry_gate, y])
+        value = layers.Add()([transformed_gated, identity_gated])
+        return value
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+    def get_config(self):
+        config = super(HighwayMulti, self).get_config()
+        config['activation'] = self.activation
+        config['transform_gate_bias'] = self.transform_gate_bias
+        return config
+
+
+class HighwayResnetDecoder3D(layers.Layer):
+    def __init__(self, filters, strides):
+        super().__init__()
+
+        self.filters = filters
+        self.conv_before_trans = layers.Conv3D(filters=filters,
+                                               kernel_size=1, padding="same",
+                                               strides=1, use_bias=USE_CONV_BIAS)
+        self.conv_trans = layers.Conv3DTranspose(filters=filters,
+                                                 kernel_size=3, padding="same",
+                                                 strides=strides, use_bias=USE_CONV_BIAS)
+        self.conv_after_trans = layers.Conv3D(filters=filters,
+                                              kernel_size=1, padding="same",
+                                              strides=1, use_bias=USE_CONV_BIAS)
+
+        self.conv_before_upsample = layers.Conv3D(filters=filters,
+                                                  kernel_size=1, padding="same",
+                                                  strides=1, use_bias=USE_CONV_BIAS)
+        self.upsample_layer = layers.UpSampling3D(size=strides)
+        self.conv_after_upsample = layers.Conv3D(filters=filters,
+                                                 kernel_size=1, padding="same",
+                                                 strides=1, use_bias=USE_CONV_BIAS)
+
+        self.norm_layer = layers.LayerNormalization(axis=-1)
+        self.act_layer = tanh
+        self.highway_layer = HighwayMulti(dim=filters)
+
+    def call(self, input_tensor):
+
+        conv_trans = self.conv_before_trans(input_tensor)
+        conv_trans = self.conv_trans(conv_trans)
+        conv_trans = self.conv_after_trans(conv_trans)
+
+        upsamle = self.conv_before_upsample(input_tensor)
+        upsamle = self.upsample_layer(upsamle)
+        upsamle = self.conv_after_upsample(upsamle)
+
+        output = self.highway_layer(conv_trans, upsamle)
+        output = self.norm_layer(output)
+        output = self.act_layer(output)
+        return output
+
+
+class OutputLayer(layers.Layer):
+    def __init__(self, last_channel_num, act="tanh", use_highway=True):
+        super().__init__()
+        self.use_highway = use_highway
+
+        self.conv_1x1 = layers.Conv3D(filters=last_channel_num,
+                                      kernel_size=1,
+                                      padding="same",
+                                      strides=1,
+                                      use_bias=USE_CONV_BIAS,
+                                      )
+        self.conv_3x3 = layers.Conv3D(filters=last_channel_num,
+                                      kernel_size=3,
+                                      padding="same",
+                                      strides=1,
+                                      use_bias=USE_CONV_BIAS,
+                                      )
+        self.conv_1x1x3 = layers.Conv3D(filters=last_channel_num,
+                                        kernel_size=(1, 1, 3),
+                                        padding="same",
+                                        strides=1,
+                                        use_bias=USE_CONV_BIAS,
+                                        )
+        self.conv_1x3x1 = layers.Conv3D(filters=last_channel_num,
+                                        kernel_size=(1, 3, 1),
+                                        padding="same",
+                                        strides=1,
+                                        use_bias=USE_CONV_BIAS,
+                                        )
+        self.conv_3x1x1 = layers.Conv3D(filters=last_channel_num,
+                                        kernel_size=(3, 1, 1),
+                                        padding="same",
+                                        strides=1,
+                                        use_bias=USE_CONV_BIAS,
+                                        )
+
+        self.act = layers.Activation(act)
+
+    def call(self, input_tensor):
+        conv_1x1 = self.conv_1x1(input_tensor)
+        conv_3x3 = self.conv_3x3(input_tensor)
+        conv_1x1x3 = self.conv_1x1x3(input_tensor)
+        conv_1x3x1 = self.conv_1x3x1(input_tensor)
+        conv_3x1x1 = self.conv_3x1x1(input_tensor)
+        output = conv_1x1 + conv_3x3 + conv_1x1x3 + conv_1x3x1 + conv_3x1x1
+        output = self.act(output)
+
+        return output
---
+++ src/model/inception_resnet_v2/x2ct.py
@@ -0,0 +1,65 @@
+from .base_model import SegInceptionResNetV2
+from .layers import SkipUpsample3D, HighwayResnetDecoder3D, OutputLayer
+from .layers import inception_resnet_block_3d, conv3d_bn
+from tensorflow.keras import Model, layers
+from tensorflow.keras import backend
+SKIP_CONNECTION_LAYER_NAMES = ["conv_down_1_ac",
+                               "maxpool_1", "maxpool_2", "mixed_6a", "mixed_7a"]
+
+
+def get_x2ct_model(xray_shape, ct_series_shape,
+                   decode_init_filter=64,
+                   last_channel_activation="tanh"):
+
+    base_model = SegInceptionResNetV2(
+        include_top=False,
+        weights=None,
+        input_tensor=None,
+        input_shape=xray_shape,
+        classes=None,
+        pooling=None,
+        classifier_activation=None
+    )
+    # x.shape: [B, 16, 16, 1536]
+    base_input = base_model.input
+    base_output = base_model.output
+    skip_connection_outputs = [base_model.get_layer(layer_name).output
+                               for layer_name in SKIP_CONNECTION_LAYER_NAMES]
+
+    ct_start_channel = 16
+    # x.shape: [B, 16, 16, 16, 1536]
+    x = SkipUpsample3D(filters=1536)(base_output, ct_start_channel)
+    for block_idx in range(1, 6):
+        x = inception_resnet_block_3d(
+            x, scale=0.17, block_type='block35_3d', block_idx=block_idx)
+    for block_idx in range(1, 6):
+        x = inception_resnet_block_3d(
+            x, scale=0.1, block_type='block17_3d', block_idx=block_idx)
+    for block_idx in range(1, 6):
+        x = inception_resnet_block_3d(
+            x, scale=0.2, block_type='block8_3d', block_idx=block_idx)
+
+    if ct_series_shape == (256, 256, 256):
+        decode_start_index = 1
+    elif ct_series_shape == (128, 128, 128):
+        decode_start_index = 2
+    else:
+        NotImplementedError(
+            "ct_series_shape is implemented only 128 or 256 intercubic shape")
+
+    init_filter = decode_init_filter
+    ct_dim = ct_start_channel
+    for index, decode_i in enumerate(range(decode_start_index, 5)):
+        current_filter = init_filter // (2 ** decode_i)
+        x = conv3d_bn(x, current_filter, 3)
+        skip_connect = skip_connection_outputs[4 - index]
+        skip_connect = SkipUpsample3D(current_filter)(skip_connect, ct_dim)
+        x = layers.Concatenate(axis=-1)([x, skip_connect])
+        x = HighwayResnetDecoder3D(current_filter,
+                                   strides=(2, 2, 2))(x)
+        ct_dim *= 2
+
+    output_tensor = OutputLayer(last_channel_num=1,
+                                act=last_channel_activation)(x)
+    output_tensor = backend.squeeze(output_tensor, axis=-1)
+    return Model(base_input, output_tensor)
